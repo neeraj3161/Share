f34d3284b51f9a3787344a20501aac3f2e07eded
Neeraj Tripathi <93697901+neeraj3161@users.noreply.github.com>
2022-05-21 11:04:28 +0530

    Add files via upload

---
+++ javaScript/date.js
@@ -0,0 +1,23 @@
+// document.querySelector(".display-date").innerHTML;
+
+// prints
+
+//printing the Date() function which is pre defined in java script
+// console.log(Date())
+
+var myname = "Neeraj"
+
+
+
+
+{/* <p class="display-date">This is a unchanged paragraph</p> */}
+
+
+var textword = document.querySelector(".display-date").innerHTML;
+
+
+console.log(textword.length);
+
+if(textword.length>28){
+    document.querySelector(".display-date").innerHTML="This is greater than 28"
+}
